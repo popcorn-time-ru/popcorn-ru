1797f1980e4a62e6e3fe0cb82b61b209c4410f2b
Ivan Borzenkov <ivan.borzenkov@gmail.com>
2019-12-15 06:13:49 +0300

    add queue metrics

--- src/Controller/MetricsController.php
+++ src/Controller/MetricsController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Repository\MovieRepository;
 use App\Repository\ShowRepository;
 use App\Repository\TorrentRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query\ResultSetMapping;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,6 +21,9 @@ class MetricsController
     /** @var TorrentRepository */
     private $torrent;
 
+    /** @var EntityManagerInterface */
+    private $em;
+
     /**
      * DatabaseMetrics constructor.
      *
@@ -27,6 +32,7 @@ class MetricsController
      * @param TorrentRepository $torrent
      */
     public function __construct(
+        EntityManagerInterface $em,
         ShowRepository $show,
         MovieRepository $movie,
         TorrentRepository $torrent
@@ -35,6 +41,7 @@ class MetricsController
         $this->show = $show;
         $this->movie = $movie;
         $this->torrent = $torrent;
+        $this->em = $em;
     }
 
     /**
@@ -60,6 +67,38 @@ class MetricsController
         foreach($this->torrent->getStatByProvider() as $provider => $count) {
             $content .= 'app_torrent{provider="'.$provider.'"} ' . $count . PHP_EOL;
         }
+
+        $rsm = new ResultSetMapping();
+        $rsm->addScalarResult('topic', 'topic');
+        $rsm->addScalarResult('spider', 'spider');
+        $rsm->addScalarResult('c', 'count');
+        $result = $this->em->createNativeQuery(
+        "select 
+            count(*) c, 
+            json_extract(properties, '$.\"enqueue.topic\"') topic, 
+            json_extract(body, '$.spider') spider 
+            from enqueue 
+            group by topic, spider",
+            $rsm
+        )->getArrayResult();
+
+        $summ = 0;
+        $content.= '# Help app_queue Len of queue'.PHP_EOL;
+        $content.= '# TYPE app_queue gauge'.PHP_EOL;
+        foreach ($result as $row) {
+            $topic = trim($row['topic'],'"');
+            if ($row['spider']) {
+                $spider = trim($row['spider'],'"');
+                $content .= 'app_queue{topic="'.$topic.'", spider="'.$spider.'"} ' . $row['count'] . PHP_EOL;
+            } else {
+                $content .= 'app_queue{topic="'.$topic.'"} ' . $row['count'] . PHP_EOL;
+            }
+        }
+
+        $content.= '# Help app_queue_all All Queue count'.PHP_EOL;
+        $content.= '# TYPE app_queue_all gauge'.PHP_EOL;
+        $content.= 'app_queue_all '.$summ.PHP_EOL;
+
         return new Response($content, 200, ['Content-Type' => 'text/plain']);
     }
 }
