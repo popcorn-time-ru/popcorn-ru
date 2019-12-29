c0f4078d87cd58a689b4c579d0f75b33a4fd4714
Ivan Borzenkov <ivan.borzenkov@gmail.com>
2019-12-29 05:36:33 +0300

    add context processor

---
+++ src/Logging/SentryContextProcessor.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Logging;
+
+use Monolog\Processor\ProcessorInterface;
+
+class SentryContextProcessor implements ProcessorInterface
+{
+    /**
+     * @param  array $record
+     * @return array
+     */
+    public function __invoke(array $record)
+    {
+        foreach ($record['context'] as $key => $val) {
+            if ($key === 'extra') {
+                continue;
+            }
+            $record['context']['extra'][$key] = is_scalar($val) ? $val : json_encode($val);
+        }
+        return $record;
+    }
+}
