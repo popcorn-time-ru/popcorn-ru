e6c523fd652ef3982f8a3ca953a1e764b0248fef
Ivan Borzenkov <ivan.borzenkov@gmail.com>
2021-04-02 02:12:22 +0300

    cache 404

---
+++ src/EventListener/CacheErrorListener.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class CacheErrorListener implements EventSubscriberInterface
+{
+    const CACHE_TIME = 3600 * 12;
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::EXCEPTION => 'Mark404Cached',
+        ];
+    }
+
+    public function Mark404Cached(ExceptionEvent $event)
+    {
+        if ($event->getThrowable() instanceof NotFoundHttpException) {
+            $response = new JsonResponse(array('massage' => "Not Found", 'code' => 404), 404);
+            $response->setSharedMaxAge(self::CACHE_TIME);
+            $event->setResponse($response);
+        }
+    }
+}
