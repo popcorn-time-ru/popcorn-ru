13c30d69c61528e50745f0204517513165d0b698
Ivan Borzenkov <ivan.borzenkov@gmail.com>
2021-04-17 10:50:08 +0300

    update tracker list

--- src/Spider/Yts.php
+++ src/Spider/Yts.php
@@ -117,14 +117,22 @@ class Yts extends AbstractSpider
         $url = 'magnet:?xt=urn:btih:' . $torrentData['hash'] . '&' . implode('&', array_map(function ($item) {
             return 'tr=' . $item;
         }, [
-            'udp://open.demonii.com:1337/announce',
-            'udp://tracker.openbittorrent.com:80',
+            'udp://tracker.opentrackr.org:1337',
+            'udp://tracker.tiny-vps.com:6969',
+            'udp://tracker.openbittorrent.com:1337',
             'udp://tracker.coppersurfer.tk:6969',
-            'udp://glotorrents.pw:6969/announce',
-            'udp://tracker.opentrackr.org:1337/announce',
-            'udp://torrent.gresille.org:80/announce',
-            'udp://p4p.arenabg.com:1337',
             'udp://tracker.leechers-paradise.org:6969',
+            'udp://p4p.arenabg.ch:1337',
+            'udp://p4p.arenabg.com:1337',
+            'udp://tracker.internetwarriors.net:1337',
+            'udp://9.rarbg.to:2710',
+            'udp://9.rarbg.me:2710',
+            'udp://exodus.desync.com:6969',
+            'udp://tracker.cyberia.is:6969',
+            'udp://tracker.torrent.eu.org:451',
+            'udp://open.stealth.si:80',
+            'udp://tracker.moeking.me:6969',
+            'udp://tracker.zerobytes.xyz:1337',
         ]));
 
         $newTorrent = new MovieTorrent();
