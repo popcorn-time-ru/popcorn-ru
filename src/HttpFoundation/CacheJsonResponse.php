d651a7a5c750671820f36e97da34cfcfd88f7c03
Ivan Borzenkov <ivan.borzenkov@gmail.com>
2021-10-13 23:31:24 +0300

    random cache time

--- src/HttpFoundation/CacheJsonResponse.php
+++ src/HttpFoundation/CacheJsonResponse.php
@@ -6,12 +6,13 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class CacheJsonResponse extends JsonResponse
 {
-    const CACHE_TIME = 3600 * 12;
+    const CACHE_TIME_MIN = 3600 * 10;
+    const CACHE_TIME_MAX = 3600 * 12;
 
     public function __construct($data = null, bool $json = false)
     {
         $this->encodingOptions = JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES;
         parent::__construct($data, 200, [], $json);
-        $this->setSharedMaxAge(self::CACHE_TIME);
+        $this->setSharedMaxAge(random_int(self::CACHE_TIME_MIN, self::CACHE_TIME_MAX));
     }
 }
