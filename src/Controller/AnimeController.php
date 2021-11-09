9f069a7becf9e1e4d8730ecc9d17a41fc3197f94
Ivan Borzenkov <ivan.borzenkov@gmail.com>
2021-11-10 00:59:17 +0300

    anime stub

---
+++ src/Controller/AnimeController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\HttpFoundation\CacheJsonResponse;
+use App\Request\LocaleRequest;
+use App\Request\PageRequest;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AnimeController extends AbstractController
+{
+    /**
+     * @Route("/animes/stat", name="animes_stat")
+     * @ParamConverter(name="localeParams", converter="locale_params")
+     */
+    public function stat(LocaleRequest $localeParams)
+    {
+        $data = [
+            'all' => [
+                'count' => 0,
+                'title' => 'All',
+            ]
+        ];
+
+        return new CacheJsonResponse($data, false);
+    }
+
+    /**
+     * @Route("/animes/{page}", name="animes_page")
+     * @ParamConverter(name="pageParams", converter="page_params")
+     * @ParamConverter(name="localeParams", converter="locale_params")
+     */
+    public function page(PageRequest $pageParams, LocaleRequest $localeParams)
+    {
+        return new CacheJsonResponse([]);
+    }
+
+}
